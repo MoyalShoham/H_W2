353ccc4d289c42e27b3d25c613107f234c3de68d
MoyalShoham <79976728+MoyalShoham@users.noreply.github.com>
2021-04-12 20:42:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,3 +62,56 @@ def LUsplit(matrix):
 
 
 result = LUsplit(A)
+
+
+
+
+#ex 1:
+def decRows(matrix, scalar, index, less):
+       for i in range(0, len(matrix)):
+             matrix[less][i] -= scalar*matrix[index][i]
+       return matrix
+
+
+def gausElimination(matrix, B):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            if i == j and matrix[i][j] is not 0:
+                for k in range(i+1,len(matrix)):
+                    scalar=0
+                    if matrix[k][j]==0:continue
+                    else:
+                        scalar = matrix[k][j]/matrix[i][j]
+                    if scalar is not 0:
+                        matrix = decRows(matrix, scalar, i, k)
+                        B[k] -= scalar*B[i]
+    return matrix
+
+
+def results(matrix, B):
+    x = [0, 0, 0]
+    x[2] = B[2]/matrix[2][2]#solve the third
+    x[1] = (B[1]-matrix[1][2]*x[2])/matrix[1][1]#solve the second using the third result
+    x[0] = (B[0]-matrix[0][1]*x[1]-matrix[0][2]*x[2])/matrix[0][0]#solve the first using all the result
+    return x
+
+
+
+I = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+#matrix = [[1, 2, 1], [2, 6, 1], [1, 1, 4]]
+matrix = [[-1.41, 2, 0], [0, 2, -1.41], [1, -1.41, 1]]
+# matrix = [[-1.41, 2, 0], [0, 2, -1.41], [0, 0, 1.01]]
+
+B = [1, 1, 1]
+for i in matrix:
+         print(i)
+
+print()
+matrix = gausElimination(matrix,B)
+for i in matrix:
+         print(i)
+
+print("result")
+print(B)
+print()
+print(results(matrix, B))
